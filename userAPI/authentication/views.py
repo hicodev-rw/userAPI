62b900d0cdec589317b8b20bf2756bca6f263f4b
hirwacl98 <hirwajeanclaude1@gmail.com>
2022-07-13 17:15:18 +0200

    update

--- userAPI/authentication/views.py
+++ userAPI/authentication/views.py
@@ -23,19 +23,19 @@ def signup(request):
         if User.objects.filter(username=username):
             messages.error(request, "username already exists")
             
-        if User.objects.filter(email=email):
+        elif User.objects.filter(email=email):
             messages.error(request, "Email already exists")
             
-        if password!=cpassword:
+        elif password!=cpassword:
             messages.error(request, "Password mismatch")
             
-        if len(password)<8 or len(password)>16:
+        elif len(password)<8 or len(password)>16:
             messages.error(request, "Password must be 8-16 character long")
             
-        if len(username)>10:
+        elif len(username)>10:
             messages.error(request, "Username is too large")
             
-        if len(username)<5:
+        elif len(username)<5:
             messages.error(request, "Username is too small")
         
         else:
@@ -43,7 +43,7 @@ def signup(request):
             myuser.first_name = firstname
             myuser.last_name = lastname
             myuser.save()
-            messages.success(request, "Your account has been successfully created!")
+            messages.success(request, "Your account has been successfully created. we have sent you a confirmation email, please confirm your email in order to activate your account!")
             
             # welcome message
             subject = "Welcome to bytecode velocity - login"
@@ -69,7 +69,7 @@ def signin(request):
             return render(request, 'index.html', {'name':firstname})
         else:
             messages.error(request, "Bad Confidential!")
-            return redirect('home')
+            
     return render(request, "signin.html")
 
 
--- userAPI/userAPI/info.py
+++ userAPI/userAPI/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = "bvtech250@gmail.com"
-EMAIL_HOST_PASSWORD = 'bvTech'
+EMAIL_HOST_PASSWORD = 'bvTech.ru'
 EMAIL_PORT = 587
